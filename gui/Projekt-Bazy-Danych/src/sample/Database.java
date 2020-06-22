c8389532c770ed54e9d94a37c2f8fc7fcfb5cef2
dydzia04 <PDyndal@gmail.com>
2020-06-22 11:26:46 +0200

    Added database connection

--- gui/Projekt-Bazy-Danych/src/sample/Database.java
+++ gui/Projekt-Bazy-Danych/src/sample/Database.java
@@ -2,28 +2,50 @@ package sample;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import java.sql.Connection;
 
 public class Database {
-    private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-            .configure() // configures settings from hibernate.cfg.xml
-            .build();
+    private final Configuration configuration = new Configuration().configure();
 
     public void test_connect(){
         try {
-            SessionFactory factory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-            Session session = factory.openSession();
+            SessionFactory sessionFactory = configuration.buildSessionFactory();
+            Session session = sessionFactory.openSession();
+            session.beginTransaction();
 
-            session.close();
-            factory.close();
+            Connection connection = session.connection();
 
         } catch (Exception ex) {
-            StandardServiceRegistryBuilder.destroy(registry);
+
         }
     }
 
+    public void add_user(){
+    }
 
+    public void add_issue(){}
+
+    public void add_project(){}
+
+    public void close_project(){}
+
+    public void edit_issues_opis(){}
+
+    public void edit_issues_podsumowanie(){}
+
+    public void edit_issues_tytul(){}
+
+    public void edit_project_opis(){}
+
+    public void edit_user_email(){}
+
+    public void edit_user_email_zgoda(){}
+
+    public void edit_user_imie_nazwisko(){}
+
+    public void edit_user_nazwa_wyswietlana(){}
 }
